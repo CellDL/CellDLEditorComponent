85cfc0c4dd557fb0bf45a1910881cfc18799b672
David Brooks <d.brooks@auckland.ac.nz>
2025-11-02 14:53:05 +1300

    A first attempt of the editor's main screen with a toolbar.

---
+++ src/components/CellDLEditor.vue
@@ -0,0 +1,72 @@
+<template>
+    <main class="editor-pane">
+        <editor-toolbar id="tool-bar"></editor-toolbar>
+
+        <div id="svg-content">
+            <context-menu id="context-menu"></context-menu>
+        </div>
+
+
+        <div id="panel-content"><!--Where an open panel is displayed--></div>
+        <cd-panel-bar id="panel-bar"></cd-panel-bar>
+    </main>
+    <footer class="status-bar">
+        <span id="status-msg"></span>
+        <span id="status-pos"></span>
+    </footer>
+</template>
+
+<script setup lang="ts">
+import editorToolbar from './EditorToolbar.vue'
+</script>
+
+
+
+<style scoped>
+.editor-pane {
+    display: flex;
+    flex: 1;
+    min-height: 0;
+    position: relative;
+}
+
+#tool-bar, #panel-bar {
+    overflow: auto;
+}
+#svg-content {
+    margin:  0;
+    border: 2px solid grey;
+    flex: 1;
+    overflow: hidden;
+}
+#panel-content {
+    padding:  6px;
+    width: 250px;
+    border: 2px solid grey;
+    display: none;
+    right: 38px; /* This depends on panel bar width... */
+    top: 0px;
+    bottom: 0px;
+    position: absolute;
+    background-color: #ECECEC;
+}
+.status-bar {
+    min-height: 1.6em;
+    border-top: 1px solid gray;
+    padding-left: 16px;
+    padding-right: 16px;
+    background-color: #ECECEC;
+}
+#status-msg.error {
+    color: red;
+}
+#status-msg.warn {
+   color: blue;
+}
+#status-pos {
+    float: right;
+}
+x-tooltip[type="error"] {
+    background: #F88;
+}
+</style>
---
+++ src/components/EditorToolbar.vue
@@ -0,0 +1,24 @@
+<template>
+    <Toolbar class="vertical">
+        <template #start>
+            <ToggleButton onIcon="pi pi-plus" offIcon="pi pi-plus" class="mr-2" />
+            <ToggleButton onIcon="pi pi-print" offIcon="pi pi-print" class="mr-2" severity="secondary" text />
+            <ToggleButton onIcon="pi pi-upload" offIcon="pi pi-upload" severity="secondary" text />
+        </template>
+    </Toolbar>
+</template>
+
+<script setup lang="ts">
+import ToggleButton from 'primevue/togglebutton'
+</script>
+
+<style>
+.p-toolbar.vertical,
+.p-toolbar.vertical > .p-toolbar-start {
+    flex-direction: column !important;
+}
+
+.p-togglebutton-label {
+    display: none;
+}
+</style>
