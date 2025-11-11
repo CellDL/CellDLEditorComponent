46664db5b75bbd24f1dd52b573678c103e505712
David Brooks <d.brooks@auckland.ac.nz>
2025-11-12 08:01:36 +1300

    Reformat `InputWidget`.

--- src/components/widgets/InputWidget.vue
+++ src/components/widgets/InputWidget.vue
@@ -38,26 +38,26 @@
 </template>
 
 <script setup lang="ts">
-import * as vue from 'vue';
+import * as vue from 'vue'
 
-import type * as locApi from '../../libopencor/locUIJsonApi';
+import type * as locApi from '../../libopencor/locUIJsonApi'
 
-const value = defineModel<number|string>({ required: true });
-const emits = defineEmits(['change']);
+const value = defineModel<number|string>({ required: true })
+const emits = defineEmits(['change'])
 const props = defineProps<{
-  maximumValue?: number;
-  minimumValue?: number;
-  name: string;
-  possibleValues?: locApi.IUiJsonDiscreteInputPossibleValue[];
-  stepValue?: number;
-}>();
+    maximumValue?: number
+    minimumValue?: number
+    name: string
+    possibleValues?: locApi.IUiJsonDiscreteInputPossibleValue[]
+    stepValue?: number
+}>()
 
 const scalarType = value.value !== '' &&
                    value.value !== null &&
                    value.value !== undefined &&
                    vue.ref<boolean>(!isNaN(+value.value))
 
-let oldValue = value.value;
+let oldValue = value.value
 
 const discreteValue = vue.computed<locApi.IUiJsonDiscreteInputPossibleValue | undefined>(() => {
     return scalarType && props.possibleValues ? props.possibleValues[<number>value.value] : undefined
@@ -68,74 +68,74 @@ const valueString = vue.computed<string>(() => String(value.value))
 // Some methods to handle a scalar value using an input text and a slider.
 
 function emitChange(newValue: number|string) {
-  void vue.nextTick().then(() => {
-    value.value = newValue;
+    void vue.nextTick().then(() => {
+        value.value = newValue
 
-    if (scalarType && props.possibleValues === undefined) {
-      scalarValue.value = <number>newValue;
-      valueString.value = String(newValue); // This will properly format the input text.
-    }
+        if (scalarType && props.possibleValues === undefined) {
+            scalarValue.value = <number>newValue
+            valueString.value = String(newValue) // This will properly format the input text.
+        }
 
-    oldValue = newValue;
+        oldValue = newValue
 
-    emits('change', props.name, newValue);
-  });
+        emits('change', props.name, newValue)
+    });
 }
 
 interface ISelectChangeEvent {
-  value: {
-    name: string;
-    value: number;
-  };
+    value: {
+        name: string
+        value: number
+    }
 }
 
 function selectChange(event: ISelectChangeEvent) {
-  if (event.value.value !== oldValue) {
-    emitChange(event.value.value);
-  }
+    if (event.value.value !== oldValue) {
+        emitChange(event.value.value)
+    }
 }
 
 function inputTextChange(newValueString: string) {
-  if (scalarType.value && newValueString === '') {
-    newValueString = String(props.minimumValue);
-  }
+    if (scalarType.value && newValueString === '') {
+        newValueString = String(props.minimumValue)
+    }
 
-  if (props.minimumValue !== undefined && Number(newValueString) < props.minimumValue) {
-    newValueString = String(props.minimumValue);
-  }
+    if (props.minimumValue !== undefined && Number(newValueString) < props.minimumValue) {
+        newValueString = String(props.minimumValue)
+    }
 
-  if (props.maximumValue !== undefined && Number(newValueString) > props.maximumValue) {
-    newValueString = String(props.maximumValue);
-  }
+    if (props.maximumValue !== undefined && Number(newValueString) > props.maximumValue) {
+        newValueString = String(props.maximumValue)
+    }
 
-  const newValue = scalarType.value ? Number(newValueString) : newValueString;
+    const newValue = scalarType.value ? Number(newValueString) : newValueString
 
-  if (newValue !== oldValue) {
-    emitChange(newValue);
-  }
+    if (newValue !== oldValue) {
+        emitChange(newValue)
+    }
 }
 
 function inputTextFocusOut(event: Event) {
-  inputTextChange((event.target as HTMLInputElement).value);
+    inputTextChange((event.target as HTMLInputElement).value)
 }
 
 function inputTextKeyPress(event: KeyboardEvent) {
-  if (event.key === 'Enter') {
-    inputTextChange((event.target as HTMLInputElement).value);
-  }
+    if (event.key === 'Enter') {
+        inputTextChange((event.target as HTMLInputElement).value)
+    }
 }
 
 function sliderChange(newValue: number | number[]) {
-  const valueToEmit = Array.isArray(newValue) ? newValue[0] : newValue;
+    const valueToEmit = Array.isArray(newValue) ? newValue[0] : newValue
 
-  if (valueToEmit !== oldValue) {
-    emitChange(valueToEmit);
-  }
+    if (valueToEmit !== oldValue) {
+        emitChange(valueToEmit)
+    }
 }
 </script>
 
 <style scoped>
-  .text-input {
-    margin-bottom: 10px;
-  }
+    .text-input {
+        margin-bottom: 10px;
+    }
 </style>
