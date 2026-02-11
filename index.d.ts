c25997a007cfd9f6ee881cfed2f706166a6e1993
David Brooks <dbrnz@users.noreply.github.com>
2026-02-12 12:36:02 +1300

    api: export `CellDLEditCommand` sub-classes.

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,16 @@
-import { Component } from 'vue'
+import type { Component } from 'vue'
 
 import type { CellDLEditorProps } from './index.ts'
-export type { CellDLEditorCommand, CellDLEditorProps, EditorData, EditorState, ViewState } from './index.ts'
+
+export type { CellDLEditorCommand, CellDLEditorProps, Theme } from './index.ts'
+export type {
+    EditorEditCommand,
+    EditorExportCommand,
+    EditorFileCommand,
+    EditorSetStateCommand,
+    EditorViewCommand
+} from './index.ts'
+export type { EditorData, EditorState, ViewState } from './index.ts'
 
 export declare const CellDLEditor: Component<CellDLEditorProps>
 export default CellDLEditor
